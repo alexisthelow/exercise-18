4381aa7f32aa7c8ff7ceea35773213ebb448c4bf
Alexis Low <alexisthelow@gmail.com>
2017-08-06 20:14:50 -0600

    second commit

--- SecretWord.java
+++ SecretWord.java
@@ -8,7 +8,7 @@ public class SecretWord {
 		System.out.print("What's the secret word? ");
 		guess = keyboard.next();
 		
-		if (guess.equals(secret)) {
+		if (guess.equals(secret) || guess.equals("liberation")) {
 			System.out.println("That's correct!");
 		} else {
 			System.out.println("No, the secret word isn't \"" + guess + "\".");
